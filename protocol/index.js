7413b1e9479ad669bbfd79ebfc3ddc61d2d7d601
Sinister Light <me@sinisterlight.com>
2023-05-12 13:13:53 +0530

    json middleware

--- protocol/index.js
+++ protocol/index.js
@@ -5,6 +5,7 @@ const port = 4000;
 
 const server = express();
 
+server.use(express.json())
 server.use(express.urlencoded({ extended: true }));
 server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'ejs');
@@ -14,7 +15,12 @@ server.get("/", (req, res) => {
   res.render('pages/index')
 });
 
+
+const UserRegistry = {}
+const RelationshipTuple = []
+
 server.post("/user", (req, res) => {
+  console.log(req.body)
   res.status(200).json({})
 })
 
