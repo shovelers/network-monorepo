072b096f26fe56d902349d24aa1295f443180bf1
Prashant Mittal <pro.mit3863@gmail.com>
2023-05-12 18:01:24 +0530

    adding simple_follow application with account create and search user functionality

---
+++ apps/simple_follow/README.md
@@ -0,0 +1,16 @@
+# dcn_demo
+DID/VC contact app demo
+
+Re-Build CSS
+* after edit in ejs file, run the following command in shell
+* (in replit) ```tailwindcss build -i ./public/styles/tailwind.css -o ./public/styles/style.css --watch```
+* (on local) ```npx tailwindcss build -i ./public/styles/tailwind.css -o ./public/styles/style.css --watch```
+
+Clean DB
+* Run the following commands in console after `cmd + c` 
+* ```
+  const Client = require("@replit/database");
+  const db = new Client();
+
+  await db.empty();
+  ```
---
+++ apps/simple_follow/index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const DIDKit = require('@spruceid/didkit-wasm-node');
+
+const path = require("path");
+const port = 3002;
+
+const server = express();
+const handlesTaken = [];
+const handleDIDMap = new Map();
+
+server.use(express.urlencoded({ extended: true }));
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs');
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get("/", (req, res) => {
+  res.render('pages/index')
+});
+
+server.post("/account", async (req, res) => {
+  var handle = req.body.fhandle;
+  var handleAlreadyTaken = await handleUniqueness(handle);
+
+  if (handleAlreadyTaken == false) {
+    var result = await createAccount(handle);
+    console.log(`Account created for handle:${handle}, did:${result["did"]}`);
+    res.redirect(`profile/${handle}`);
+  } else {
+    console.log(`Handle: ${handle} already taken`);
+    res.status(404).send(`Handle: ${handle} already taken`);
+  };
+});
+
+server.post("/signin", async (req, res) => {
+  var handle = req.body.fhandle;
+  var handleAlreadyTaken = await handleUniqueness(handle);
+
+  if (handleAlreadyTaken == false) {
+    console.log("Create an account first");
+    res.status(404).send("Create an account first");
+  } else {
+    console.log(`Profile lookup for handle:${handle}`);
+    res.redirect(`profile/${handle}`);
+  };
+});
+
+
+server.get("/profile/:handle", async (req, res) => {
+  var params = req.params;
+  var handle = params["handle"];
+  //var result = await db.get(did);
+  //var followers = await db.get(`followers-${did}`);
+  //var following = await db.get(`following-${did}`);
+
+  res.render('pages/profile_v2',{
+    //did: params["id"],
+    //key: result["key"],
+    handle: handle,
+    //follower_count: followers.length,
+    //following_count: following.length
+  });
+});
+
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(
+    `> Ready on ${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
+  );
+});
+
+async function handleUniqueness(handle) {
+  return handlesTaken.includes(handle);
+  console.log(handlesTaken.includes(handle));
+};
+
+async function createAccount(handle) {
+  var key = DIDKit.generateEd25519Key();
+  handlesTaken.push(handle);
+  console.log(key);
+  //call client to generate did and pass this key
+  //handleDIDMap.set()
+
+  return {key: key}
+};
