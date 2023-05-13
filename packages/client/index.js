f54687faffe7936a3e7f2a1ce036242364e91adc
Sinister Light <me@sinisterlight.com>
2023-05-13 22:54:29 +0530

    protocol client class

--- packages/client/index.js
+++ packages/client/index.js
@@ -1,1 +1,23 @@
-module.exports = "client"
+class Protocol {
+  constructor(config) {
+    this.base_url = "http://localhost:4000"
+  }
+
+  registerUser() {
+
+  }
+
+  registerGraph() {
+
+  }
+
+  insertGraph() {
+
+  }
+
+  readGraph () {
+
+  }
+}
+
+module.exports = Protocol
