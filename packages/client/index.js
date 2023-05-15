52f3e39ccca9c70ca7d2f9e63cfe7c4c8f95e81f
Sinister Light <me@sinisterlight.com>
2023-05-15 11:32:09 +0530

    plugging api calls from rolodex to protocl

--- packages/client/index.js
+++ packages/client/index.js
@@ -8,27 +8,16 @@ class Protocol {
     })
   }
 
-  registerUser() {
-    this.axios_client.post('/user', {
-      did: "did:ion:z6MkkMKvnc3vJfQp8Sr8dYVjc6zrTQaxcDBbtYqW3wMJW2hY",
-      doc: "qwertyuiolpasdfghjkjhgfdsasdfghjmknbvcxcvbnbvcxzfghudsdfyuy"
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    console.log("registerUser")
-
-  }
-
   async generateDID(key) {
     var did = DIDKit.keyToDID('key', key);
     var doc = await DIDKit.resolveDID(did, "{}");
 
     console.log("generateDID for given key & return DID & DID doc")
     return {did: did, doc: doc}
+  }
+
+  async registerUser(did, doc) {
+    await this.axios_client.post('/user', {did: did, doc: doc})
   }
 
   registerGraph() {
