9b55339819e39cd51d05a37a406c64533728e3c9
Sinister Light <me@sinisterlight.com>
2023-05-15 11:13:48 +0530

    sample api request from client

--- packages/client/index.js
+++ packages/client/index.js
@@ -1,11 +1,24 @@
 const DIDKit = require('@spruceid/didkit-wasm-node');
+const axios = require('axios');
 
 class Protocol {
   constructor(config) {
-    this.base_url = "http://localhost:4000"
+    this.axios_client  = axios.create({
+      baseURL: "http://localhost:4000/"
+    })
   }
 
   registerUser() {
+    this.axios_client.post('/user', {
+      did: "did:ion:z6MkkMKvnc3vJfQp8Sr8dYVjc6zrTQaxcDBbtYqW3wMJW2hY",
+      doc: "qwertyuiolpasdfghjkjhgfdsasdfghjmknbvcxcvbnbvcxzfghudsdfyuy"
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
     console.log("registerUser")
 
   }
