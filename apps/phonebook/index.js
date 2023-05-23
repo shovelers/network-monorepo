cc0eea241a36c73d22f519a77b83c07e0592a843
Sinister Light <me@sinisterlight.com>
2023-05-23 14:41:17 +0530

    phonebook app to show all contacts of a did across the network

---
+++ apps/phonebook/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+const port = 3001;
+const server = express();
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs');
+server.use(express.static(path.join(__dirname, 'public')));
+
+const Protocol = require('client');
+const c = new Protocol();
+
+const apps = [
+  { name: "Rolodex",       graphDID: "did:graph:rolodex" },
+  { name: "Simple Follow", graphDID: "did:graph:simple_follow" }
+]
+
+server.get("/users/:did", async (req, res) => {
+  var socialGraph = []
+  for (const app of apps) {
+    var graphData = await c.readGraph(app.graphDID);
+    var relationships = await relationshipFor(req.params.did, graphData);
+    socialGraph.push({ name: app.name, relationships: relationships});
+  };
+
+  res.render('pages/index', {
+    socialGraph: socialGraph
+  })
+});
+
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(
+    `> Ready`
+  );
+});
+
+async function relationshipFor(did, graphData) {
+  var list = [];
+  graphData.forEach(function (item, index) {
+    if ((item['from'] == did) || (item['to'] == did))
+      list.push(item);
+  });
+  return list;
+}
