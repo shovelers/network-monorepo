cfda3fbf407af66fa7f2455c9bc0bfadbdc003f3
Prashant Mittal <pro.mit3863@gmail.com>
2023-06-03 13:59:20 +0530

    [Simple Follow] Removes DID Atuh specific JS function

--- apps/simple_follow/public/javascript/crypto_utils.js
+++ apps/simple_follow/public/javascript/crypto_utils.js
@@ -2,49 +2,6 @@ import init, { generateEd25519Key, keyToDID, resolveDID, keyToVerificationMethod
 
 await init();
 
-async function generateKeyAndDID(form) {
-  const key = JSON.parse(generateEd25519Key());
-  var did = document.getElementById('gdid').value;
-  if (did.length == 0) {
-    did = keyToDID('key', JSON.stringify(key));
-    document.getElementById('gdid').value = did;
-  }
-  const doc = await resolveDID(did, "{}");
-  document.getElementById('gdoc').value = doc;
-  console.log(`did: ${did}, key:${JSON.stringify(key)}`);
-
-  form.submit();
-}
-
-async function signChallenge(form) {
-  const key = JSON.parse(document.getElementById('gkey').value);
-  const handle = document.getElementById('ghandle').value;
-  const did = keyToDID('key', JSON.stringify(key));
-
-  //create verificationMethod using key amd did method
-  try {
-    var verificationMethod = await keyToVerificationMethod("key", JSON.stringify(key));
-  } catch(e) {
-    console.log(e);
-    return false;
-  }
-
-  //vp is the signed version of proofOptions which has the challenge string
-  var proofOptions = {
-    proofPurpose: "authentication",
-    challenge: `${handle}`,
-    verificationMethod: `${verificationMethod}`,
-  };
-  try {
-    var vp = await DIDAuth(did, JSON.stringify(proofOptions), JSON.stringify(key));
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-  document.getElementById('gvp').value = vp;
-  form.submit();
-}
-
 async function createPasskey(form) {
   var handle = document.getElementById('phandle').value;
   var res = await fetch('/did?'+ new URLSearchParams({handle: handle}));
