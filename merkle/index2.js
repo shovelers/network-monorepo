a830b50eed656463c7357f1e2884c604384ca654
Prashant Mittal <pro.mit3863@gmail.com>
2023-08-28 15:30:26 +0530

    [merkle] add index file to run another libp2p node which connects to an already running node

---
+++ merkle/index2.js
@@ -0,0 +1,145 @@
+import { createHelia } from 'helia';
+import { dagCbor } from '@helia/dag-cbor';
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { tcp } from '@libp2p/tcp'
+import { multiaddr } from 'multiaddr'
+import { MemoryBlockstore } from 'blockstore-core'
+import { MemoryDatastore } from 'datastore-core'
+import { createLibp2p } from 'libp2p'
+import { identifyService } from 'libp2p/identify'
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { keyToDID } from '@spruceid/didkit-wasm-node';
+
+const port = 4001;
+
+const server = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+server.use(express.json());
+server.use(cors());
+
+server.use(express.urlencoded({ extended: true }));
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs');
+server.use(express.static(path.join(__dirname, 'public')));
+
+const Registries = {}
+/*
+ head = {regID1to1from2: cid1
+         regID1to2from1: cid2}
+*/
+const Heads = new Map();
+
+//Node Setup
+const node = await createNode()
+const dag = await dagCbor(node)
+const multiaddrs = node.libp2p.getMultiaddrs()
+console.log("node address:", multiaddrs);
+var d = await node.libp2p.dial(multiaddr(process.argv[2]));
+console.log("dailed to:", d.remoteAddr)
+
+
+server.get("/", async (req, res) => {
+  res.render('pages/index', {})
+});
+
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(
+    `> Ready`
+  );
+});
+
+/*
+sample re.body =
+  {
+    "name": "simple_follow_follow",
+    "publickey": {"kty":"OKP","crv":"Ed25519","x":"EL_Z0oW6OLhN4Pe4LAzzGmOWkGZpxmhoqD0IAvQ4wGA"}
+  }
+  */
+server.post("/registry", async (req, res) => {
+  var registryDID = await createRegistry(req.body)
+  console.log(`registry created with did ${registryDID}`)
+  res.status(200).json({"did": registryDID})
+})
+
+/*
+ sample req.body =
+ {
+   "registryID": "did:reg",
+   "to": "did:a",
+   "from": "did:b",
+   "state": "requested",
+   "sig": "hakjshdkjasnd"
+ }
+ */
+server.post("/event", async (req, res) => {
+  var CID = await addEvent(req.body)
+  res.status(200).json({"cid": CID})
+})
+
+async function createRegistry(body) {
+  var name = body.name
+  var publickey = body.publickey
+  var did = keyToDID('key', JSON.stringify(publickey))
+
+  return did
+}
+
+async function addEvent (body) {
+  var relID = `${body.regID}` + `${body.to}` + `${body.from}`
+  if (Heads.has(relID)) {
+    var CID = Heads.get(relID);
+    console.log("Already_Present_CID:", CID)
+    const object = { event: body, link: CID };
+    const newCID = await dag.add(object);
+    Heads.set(relID, newCID);
+    console.log('newCID:', newCID)
+    var head = await dag.get(newCID)
+    console.log("head:", head)
+    console.log("prev:", await dag.get(head.link))
+    return newCID
+
+  } else {
+    const object = { event: body };
+    const CID = await dag.add(object);
+    Heads.set(relID, CID)
+    console.log("set_CID", CID)
+    console.log("first", await dag.get(CID))
+    return CID
+  }
+}
+
+async function createNode () {
+  // the blockstore is where we store the blocks that make up files
+  const blockstore = new MemoryBlockstore()
+
+  // application-specific data lives in the datastore
+  const datastore = new MemoryDatastore()
+
+  // libp2p is the networking layer that underpins Helia
+  const libp2p = await createLibp2p({
+    datastore,
+    addresses: {
+      listen: ['/ip4/127.0.0.1/tcp/0']
+    },
+    transports: [tcp()],
+    connectionEncryption: [noise()],
+    streamMuxers: [yamux()],
+    services: {
+      identify: identifyService(),
+    },
+  })
+
+  return await createHelia({
+    datastore,
+    blockstore,
+    libp2p
+  })
+}
