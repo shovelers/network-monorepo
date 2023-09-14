5057d681b82a9dd7caffc65c64d2bd6eb3ab90ff
Prashant Mittal <pro.mit3863@gmail.com>
2023-09-14 15:14:50 +0530

    [rolodex] adds odd sdk to frontend & dummy signup function to trigger odd sdk

--- apps/rolodex/src/odd.js
+++ apps/rolodex/src/odd.js
@@ -1,15 +1,13 @@
-import * as odd from '@oddjs/odd'
-
-async function signup() {
+async function signup(odd) {
   const program = await odd.program({
     // `namespace` can also be just a string; it's used as an identifier for caches.
     // If you're developing multiple apps on the same localhost port,
     // make sure these differ.
     namespace: { creator: "Nullsoft", name: "Winamp" }
-
   }).catch(error => {
     switch (error) {
       case odd.ProgramError.InsecureContext:
+
         // ODD requires HTTPS
         break;
       case odd.ProgramError.UnsupportedBrowser:
@@ -18,6 +16,7 @@ async function signup() {
     }
 
   })
+  console.log("program", program)
 }
 
 export { signup };
