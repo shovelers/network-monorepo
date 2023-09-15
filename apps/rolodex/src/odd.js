56cffaf355ba29fa47a7aedaa2c4dc9802a8f7db
Prashant Mittal <pro.mit3863@gmail.com>
2023-09-15 12:23:29 +0530

    [rolodex] On signup, create /profile/profile.json in private directory in WNFS

--- apps/rolodex/src/odd.js
+++ apps/rolodex/src/odd.js
@@ -1,42 +1,54 @@
 async function signup(odd) {
-  const program = await odd.program({
-    // `namespace` can also be just a string; it's used as an identifier for caches.
-    // If you're developing multiple apps on the same localhost port,
-    // make sure these differ.
-    namespace: { creator: "Nullsoft", name: "Winamp" }
-  }).catch(error => {
-    switch (error) {
-      case odd.ProgramError.InsecureContext:
+  const appInfo = { creator: "Shovel", name: "Rolod" }
+  const program = await odd.program({namespace: appInfo})
+    .catch(error => {
+      switch (error) {
+        case odd.ProgramError.InsecureContext:
+          // ODD requires HTTPS
+          break;
+        case odd.ProgramError.UnsupportedBrowser:
+          // Browsers must support IndexedDB
+          break;
+      }
+    })
+  console.log(program)
 
-        // ODD requires HTTPS
-        break;
-      case odd.ProgramError.UnsupportedBrowser:
-        // Browsers must support IndexedDB
-        break;
-    }
-
-  })
-  console.log("program", program)
-
+  //create session
   let session
   if (program.session) {
     session = program.session
-    await session.destroy()
+    //await session.destroy()
   }
-  console.log("I am here")
   const username = document.getElementById('handle').value
   const valid = program.auth.isUsernameValid(username)
   const available = await program.auth.isUsernameAvailable(username)
   console.log("username available", available)
 
   if (valid && available) {
+    console.log("registering ...", username)
     // Register the user
     const { success } = await program.auth.register({ username: username })
-
+    console.log("success: ", success)
     // Create a session on success
     session = success ? program.auth.session() : null
   }
   console.log("session", await session)
+
+  //create fs
+  const fs = session.fs
+  console.log(fs)
+  const profileData = JSON.stringify({"handle": username})
+
+  const { RootBranch } = odd.path
+  const privateDirectoryPath = odd.path.directory("private", "profile")
+  const privateFilePath = odd.path.file(RootBranch.Private, "profile", "profile.json")
+
+  await fs.write(privateFilePath,new TextEncoder().encode(profileData))
+  await fs.publish()
+
+  const content = new TextDecoder().decode(await fs.read(privateFilePath))
+  console.log("profile data :", content)
+
 }
 
 export { signup };
