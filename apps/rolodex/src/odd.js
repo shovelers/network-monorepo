8310533ee2165501d6d14b0e4ec7f0f491cd735c
Prashant Mittal <pro.mit3863@gmail.com>
2023-09-21 18:37:56 +0530

    [rolodex] uses render table in getContacts as well

--- apps/rolodex/src/odd.js
+++ apps/rolodex/src/odd.js
@@ -127,20 +127,20 @@ async function deleteContact(odd, id) {
 
 function renderTable(contacts) {
   let table = document.createElement('table');
-  table.classList.add("table", "table-zebra", "table-pin-rows")
+  table.classList.add("table")
   var tbody = table.createTBody();
   for (let [key, value] of Object.entries(contacts.contactList)) {
     let row = tbody.insertRow();
     let cell = row.insertCell(0);
     cell.textContent = value;
     let editcell = row.insertCell(1);
-    editcell.classList.add("text-right")
+    editcell.style.cssText = 'text-align: right';
     editcell.innerHTML = `
       <button class="btn btn-ghost btn-xs" onclick="contact_edit_modal.showModal(); setContactEditForm('${key}', '${value}');">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" d="m13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057l3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/></svg>
       </button>`
   }
-  return table;
+  return tbody;
 }
 
 async function updateFile(odd, file, mutationFunction) {
