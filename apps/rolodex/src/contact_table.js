bce4cd6f019537804f62eaf128fc709772d2085b
Prashant Mittal <pro.mit3863@gmail.com>
2023-11-02 13:23:03 +0530

    [rolodex] adds contact counter

--- apps/rolodex/src/contact_table.js
+++ apps/rolodex/src/contact_table.js
@@ -44,11 +44,11 @@ export class ContactTable extends HTMLElement {
   updateTable(contactList) {
     const table = this.querySelector('table');
     const tbody = this.querySelector('tbody');
-    
     // Clear existing rows
     while (table.rows.length > 1) {
       table.deleteRow(1);
     }
+    var rowCount = 0;
 
     // Create table rows for contacts
     for (let [key, value] of Object.entries(contactList)) {
@@ -77,7 +77,10 @@ export class ContactTable extends HTMLElement {
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" d="m13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057l3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/></svg>
         </button>`
       tbody.appendChild(row);
+      rowCount = rowCount + 1;
     };
+
+    document.getElementById('contact-count').innerHTML = rowCount;
   }
 
   sortTable(element) {
