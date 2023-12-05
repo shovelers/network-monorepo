4badd44b5d34714cbb1ff793dcdb35a8bfdf296f
Prashant Mittal <pro.mit3863@gmail.com>
2023-12-05 14:27:27 +0530

    adding helia pds test

---
+++ apps/helia_pds/public/helia_pds_browser.js
@@ -0,0 +1,41 @@
+import { createHelia } from 'helia';
+import { webSockets } from '@libp2p/websockets'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { MemoryBlockstore } from 'blockstore-core'
+import { MemoryDatastore } from 'datastore-core'
+import { createLibp2p } from 'libp2p'
+import { multiaddr } from 'multiaddr'
+
+const node = await createNode()
+const multiaddrs = node.libp2p.getMultiaddrs()
+console.log("node address:", multiaddrs);
+
+async function dial(peer){
+  await node.libp2p.dial(multiaddr(peer));
+  console.log("latency:", latency)
+};
+
+async function createNode () {
+  // the blockstore is where we store the blocks that make up files
+  const blockstore = new MemoryBlockstore()
+
+  // application-specific data lives in the datastore
+  const datastore = new MemoryDatastore()
+
+  // libp2p is the networking layer that underpins Helia
+  const libp2p = await createLibp2p({
+    datastore,
+    transports: [webSockets()],
+    connectionEncryption: [noise()],
+    streamMuxers: [yamux()],
+  })
+
+  return await createHelia({
+    datastore,
+    blockstore,
+    libp2p
+  })
+}
+
+export { createNode, dial }
---
+++ apps/helia_pds/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import cors from 'cors';
+
+const port = process.argv[2] || 3000;
+const server = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+server.use(express.json());
+server.use(cors());
+
+server.use(express.urlencoded({ extended: true }));
+server.set('views', path.join(__dirname, 'views'));
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get("/", async (req, res) => {
+});
+
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(
+    `> Ready`
+  );
+});
