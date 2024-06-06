baf8ce8ef9d7836f9ce549fe52683f488758d4f5
Prashant Mittal <pro.mit3863@gmail.com>
2024-06-06 15:06:51 +0530

    [Rolodex] community shares contacts.json also on Join handhsake confirm

--- packages/account-fs/agent/handshakes/join.js
+++ packages/account-fs/agent/handshakes/join.js
@@ -6,13 +6,18 @@ export class JoinHandshake extends Handshake {
   async confirmData() {
     let memberDirectoryAccessKey = await this.agent.getAccessKeyForPrivateFile('members.json')
     let encodedMemberDirectoryAccessKey = uint8arrays.toString(memberDirectoryAccessKey.toBytes(), 'base64url');
+    
+    let communityContactsAccessKey = await this.agent.getAccessKeyForPrivateFile('contacts.json')
+    let encodedCommunityContactsAccessKey = uint8arrays.toString(communityContactsAccessKey.toBytes(), 'base64url');
+
+    console.log("preparing community info to be shared in join handshake...")
  
     return {
       community: {
         FN: await this.agent.handle(),
         UID: `DCN:${await this.agent.accountDID()}`,
         PRODID: "DCN:rolodex",
-        XML: `members.json:${await this.agent.handle()}.${encodedMemberDirectoryAccessKey}`
+        XML: `members.json:${await this.agent.handle()}.${encodedMemberDirectoryAccessKey}|contacts.json:${await this.agent.handle()}.${encodedCommunityContactsAccessKey}`
       }
     }
   }
