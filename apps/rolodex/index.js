781c3c1a72112f754fee07f9653f5275a9aca66f
Prashant Mittal <pro.mit3863@gmail.com>
2024-06-07 22:04:54 +0530

    debug logging rolodex deployment

--- apps/rolodex/index.js
+++ apps/rolodex/index.js
@@ -44,6 +44,7 @@ if (RUN_COMMUNITY_AGENT == true) {
     
     const communityRuntimeConfig = JSON.parse(await fs.readFile(path.join(__dirname, "community_agent_runtime_config.json"), 'utf8'))
     console.log("community runtime config filecontent :", communityRuntimeConfig)
+    console.log("network config and connection object", NETWORK, connection, connection[NETWORK] )
     //add accessKey from envVar to runtime config
     communityRuntimeConfig.SHOVEL_FS_ACCESS_KEY = COMMUNITY_AGENT_ACCESS_KEY
     //add forestCID from hub to runtime config
