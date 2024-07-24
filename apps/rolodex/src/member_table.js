43d9565fececc07469761ddfbab69bacde385fbb
Prashant Mittal <pro.mit3863@gmail.com>
2024-07-24 13:13:06 +0530

    [Rolodex] memberTable uses dynamic values to generate columns and filter based on passed options

--- apps/rolodex/src/member_table.js
+++ apps/rolodex/src/member_table.js
@@ -3,16 +3,14 @@
   constructor() {
     super();
 
-    this.filters = {
-      lookingFor: [],
-      canHelpWith: [],
-      expertise: []
-    };
-    this.filterOptions = {
-      lookingFor: ['Gigs', 'Warm Intros', 'Job', 'Partnerships', 'Talent'],
-      canHelpWith: ['Testing', 'Design', 'Tokenomics', 'Job/Gig Opportunities', 'GTM', 'Fundraise', 'Introductions', 'Ideation', 'Mentorship','Development'],
-      expertise: ['Full Stack', 'Backend', 'Frontend', 'Design', 'Consumer Tech', 'Smart Contracts', 'Frames', 'Data Analysis', 'Community', 'Social']
-    };
+    this.options = JSON.parse(this.getAttribute("data-options")) || {};
+    this.filters = {}
+    this.filterOptions = {}
+    // Initialize filters and filterOptions based on the provided options
+    Object.keys(this.options).forEach(key => {
+      this.filters[key] = [];
+      this.filterOptions[key] = this.options[key].symbols;
+    });
    
     const container = document.createElement('div');
     container.classList.add('relative');
@@ -28,7 +26,7 @@
     const thead = table.createTHead();
     const headerRow = document.createElement('tr');
     headerRow.classList.add('grid', this.gridSize); 
-    const headers = ['Name (sort)', 'Looking For', 'Can Help With', 'Expertise']; // Customize as needed
+    const headers = ['Name (sort)', ...Object.keys(this.options).map(key => this.options[key].label)];
     headers.forEach((headerText, index) => {
       const th = document.createElement('th');
       th.classList.add('cursor-pointer','col-span-2', 'relative') //add value for 'col-span-X'  based on corresponding row element size
@@ -61,7 +59,7 @@
       if (index > 0) {
         const filterDropdown = document.createElement('div');
         filterDropdown.classList.add('filter-dropdown', 'hidden', 'fixed', 'border', 'border-gray-300', 'shadow-lg', 'z-50', 'p-2','max-h-[80vh]', 'overflow-y-auto', 'bg-[#0f1729]');
-        this.createFilterOptions(filterDropdown, headerText.replace(/\s/g, ''));
+        this.createFilterOptions(filterDropdown, Object.keys(this.options)[index - 1]);
         
         document.body.appendChild(filterDropdown);  // Append to body
         
@@ -108,11 +106,10 @@
 
   // New method to create filter options
   createFilterOptions(dropdown, column) {
-    dropdown.setAttribute('data-header', column);
+    dropdown.setAttribute('data-header', this.options[column].label);
     // Convert column to lowercase for case-insensitive matching
     const lowercaseColumn = column.toLowerCase();
     
-    console.log('filter options', this.filterOptions);
     if (!this.filterOptions) {
       console.error('Filter options are undefined in createFilterOptions');
       return;
@@ -160,8 +157,7 @@
     const spans = document.querySelectorAll('.flex.items-center.justify-between span');
     spans.forEach((span) => {
       const spanText = span.textContent.replace(/\s/g, '').toLowerCase();
-      console.log(spanText)
-        if (spanText === column.toLowerCase()) {
+      if (spanText === column.toLowerCase()) {
         const flexParent = span.closest('.flex');
         const filterIcon = flexParent.querySelector('.filter-icon');
         if (this.filters[column].length > 0) {
@@ -171,12 +167,13 @@
           filterIcon.setAttribute('fill', 'none');
         }
       }
-      });  
-    }
+    });  
+  }
 
-    isTagHighlighted(column, tag) {
-      return this.filters[column].includes(tag.toLowerCase());
-    }      
+  isTagHighlighted(column, tag) {
+    return this.filters[column].includes(tag.toLowerCase());
+  }
+
   updateFilter(column, value, checked) {
     if (checked) {
       this.filters[column].push(value);
@@ -186,6 +183,7 @@
     this.updateFilterButtonAppearance(column);
     this.applyFilters();
   }
+
   handleWindowResize() {
     document.querySelectorAll('.filter-dropdown:not(.hidden)').forEach(dropdown => {
       const th = this.findAssociatedTh(dropdown);
@@ -196,7 +194,6 @@
   }
 
   applyFilters() {
-    console.log('this member and this filter', this._members, this.filter);
     if (!this.filters) {
       console.error('Filters are undefined in applyFilter');
       return;
@@ -216,7 +213,8 @@
 
   findAssociatedTh(dropdown) {
     const headerText = dropdown.getAttribute('data-header');
-    return this.querySelector(`th:nth-child(${['Name (sort)', 'Looking For', 'Can Help With', 'Expertise'].indexOf(headerText) + 1})`);
+    const headers = ['Name (sort)', ...Object.keys(this.options).map(key => this.options[key].label)];
+    return this.querySelector(`th:nth-child(${headers.indexOf(headerText) + 1})`);
   }
   
   set members(value) {
@@ -247,52 +245,20 @@
       const nameCell = row.insertCell(0);
       nameCell.classList.add('flex', 'flex-row', 'justify-between', 'w-full', 'items-center', 'col-span-2', 'font-medium');
       nameCell.innerHTML = `<div class="avatar"><div class="w-8 rounded-full"><img src="${value.pfpUrl}" onerror="this.onerror=null;this.src='https://i.imgur.com/tmGAd6X.jpg';"/></div></div> <span class="text-center">${value.name}</span> <a href="https://warpcast.com/${value.handle}" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M18.24.24H5.76A5.76 5.76 0 0 0 0 6v12a5.76 5.76 0 0 0 5.76 5.76h12.48A5.76 5.76 0 0 0 24 18V6A5.76 5.76 0 0 0 18.24.24m.816 17.166v.504a.49.49 0 0 1 .543.48v.568h-5.143v-.569A.49.49 0 0 1 15 17.91v-.504c0-.22.153-.402.358-.458l-.01-4.364c-.158-1.737-1.64-3.098-3.443-3.098c-1.804 0-3.285 1.361-3.443 3.098l-.01 4.358c.228.042.532.208.54.464v.504a.49.49 0 0 1 .543.48v.568H4.392v-.569a.49.49 0 0 1 .543-.479v-.504c0-.253.201-.454.454-.472V9.039h-.49l-.61-2.031H6.93V5.042h9.95v1.966h2.822l-.61 2.03h-.49v7.896c.252.017.453.22.453.472"/></svg></a>`;
-
-      const lookingForCell = row.insertCell(1); 
-      lookingForCell.classList.add('col-span-2', 'font-medium', 'space-x-1')
-      if (value.lookingFor !== undefined && value.lookingFor.length > 0) {
-        let tags = Array.isArray(value.lookingFor) ? value.lookingFor : [value.lookingFor];  // Check if EMAIL is an array, if not, treat it as a single string in an array
-        for (let tag of tags) {
-          const badgeClass = this.isTagHighlighted('lookingFor', tag) ? 'border-1 border-white' : '';
-          lookingForCell.innerHTML += `<span class="badge badge-md badge-secondary ${badgeClass}">${tag}</span>`;
+      
+      Object.keys(this.options).forEach(optionKey => {
+        const cell = row.insertCell();
+        cell.classList.add('col-span-2', 'font-medium', 'space-x-1');
+        if (value[optionKey] !== undefined && value[optionKey].length > 0) {
+          let tags = Array.isArray(value[optionKey]) ? value[optionKey] : [value[optionKey]];
+          for (let tag of tags) {
+            const badgeClass = this.isTagHighlighted(optionKey, tag) ? 'border-1 border-white' : '';
+            cell.innerHTML += `<span class="badge badge-md badge-secondary ${badgeClass}">${tag}</span>`;
+          }
+        } else {
+          cell.innerHTML = '';
         }
-      } else {
-        lookingForCell.innerHTML = '';
-      }
-      const canHelpWithCell = row.insertCell(2); 
-      canHelpWithCell.classList.add('col-span-2', 'font-medium', 'space-x-1')
-      if (value.canHelpWith !== undefined && value.canHelpWith.length > 0) {
-        let tags = Array.isArray(value.canHelpWith) ? value.canHelpWith : [value.canHelpWith];  // Check if EMAIL is an array, if not, treat it as a single string in an array
-        for (let tag of tags) {
-          const badgeClass = this.isTagHighlighted('canHelpWith', tag) ? 'border-1 border-white' : '';
-          canHelpWithCell.innerHTML += `<span class="badge badge-md badge-secondary ${badgeClass}">${tag}</span>`;
-        }
-      } else {
-        canHelpWithCell.innerHTML = '';
-      }
-      const expertiseCell = row.insertCell(3); 
-      expertiseCell.classList.add('col-span-2', 'font-medium', 'space-x-1')
-      if (value.expertise !== undefined && value.expertise.length > 0) {
-        let tags = Array.isArray(value.expertise) ? value.expertise : [value.expertise];  // Check if EMAIL is an array, if not, treat it as a single string in an array
-        for (let tag of tags) {
-          const badgeClass = this.isTagHighlighted('expertise', tag) ? 'border-1 border-white' : '';
-          expertiseCell.innerHTML += `<span class="badge badge-md badge-secondary ${badgeClass}">${tag}</span>`;
-        }
-      } else {
-        expertiseCell.innerHTML = '';
-      }
-     // let editcell = row.insertCell(3);
-     // editcell.classList.add('justify-self-end', 'col-span-1');
-     // if (value.XML && value.XML.split(':')[0] == 'via') {
-     //   editcell.innerHTML = `<span class="badge badge-secondary">${value.XML}</span>`
-     // } else if (value.XML && value.XML.split(':')[0] != 'via') {
-     //   editcell.innerHTML = `<span class="badge badge-primary">${value.PRODID.split(':')[1]}</span>`
-     // } else {
-     //   editcell.innerHTML = `
-     //   <button class="btn btn-ghost btn-xs" onclick="contact_edit_modal.showModal(); setContactEditForm('${value.UID}');">
-     //     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" d="m13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057l3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/></svg>
-     //   </button>`
-     // }
+      });
       tbody.appendChild(row);
       rowCount = rowCount + 1;
     };
@@ -331,7 +297,7 @@
 
   resetFilters() {
     // Reset all filters
-    Object.keys(this.filters).forEach(key => {
+    Object.keys(this.options).forEach(key => {
       this.filters[key] = [];
     });
   
