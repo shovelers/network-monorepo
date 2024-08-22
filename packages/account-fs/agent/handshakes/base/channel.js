104f8c04fd6fd0cc227f46cae088fd7d8fbeeeea
Sinister Light <me@sinisterlight.com>
2024-08-22 20:05:14 +0530

    retries for forwarding channel

--- packages/account-fs/agent/handshakes/base/channel.js
+++ packages/account-fs/agent/handshakes/base/channel.js
@@ -17,31 +17,34 @@ export class Channel {
     this.helia.libp2p.services.pubsub.subscribe(this.name)
   }
 
-  async publish(message, attempt=0) {
-    try {
-      await this.helia.libp2p.services.pubsub.publish(this.name, new TextEncoder().encode(message))
-      console.log(message)
-    } catch (e) {
-      // TODO - the error name has changed in gossipsub 13.x, we are on 11.x - fix on upgrade
-      if (e.message == "PublishError.InsufficientPeers") {
-        console.log(`Publish failed to ${this.name}. Error ${e.message}. Retrying`)
-        if (attempt < 5) {
-          await new Promise(resolve => setTimeout(resolve, 20))
-          await this.publish(message, attempt + 1)
-        } else throw e
-      } else {
-        throw e
-      }
-    }
+  async publish(message) {
+    await this._publish(this.name, message)
   }
 
   async publishViaForwarder(message) {
     if (this.forwardingChannel) {
       const packet = JSON.stringify({topic: this.name, message: JSON.parse(message)})
-      this.helia.libp2p.services.pubsub.publish(this.forwardingChannel, new TextEncoder().encode(packet))
-      console.log(packet)
+      await this._publish(this.forwardingChannel, packet)
     } else {
       await this.publish(message)
+    }
+  }
+
+  async _publish(topic, message, attempt=0) {
+    try {
+      await this.helia.libp2p.services.pubsub.publish(topic, new TextEncoder().encode(message))
+      console.log(message)
+    } catch (e) {
+      // TODO - the error name has changed in gossipsub 13.x, we are on 11.x - fix on upgrade
+      if (e.message == "PublishError.InsufficientPeers") {
+        console.log(`Publish failed to ${topic}. Error ${e.message}. Retrying`)
+        if (attempt < 5) {
+          await new Promise(resolve => setTimeout(resolve, 20))
+          await this._publish(topic, message, attempt + 1)
+        } else throw e
+      } else {
+        throw e
+      }
     }
   }
 }
